584459343ca90f277817f18bd1b5e47b9b3baf46
d-e-e-p-mandal <deepmandal9091@gmail.com>
2024-11-17 14:55:12 +0530

    Add files via upload

---
+++ LOOP CONSTRUCT/q3_note_01_multiplication_table.c
@@ -0,0 +1,14 @@
+// multiplication table
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("enter number \n");
+    scanf("%d", &num);
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("%d*%d=%d\n", num, i, num * i);
+    }
+
+    return 0;
+}
