0a15cd90d53ee8ecceb7cef6786a61c481f92357
d-e-e-p-mandal <deepmandal9091@gmail.com>
2024-11-19 00:55:10 +0530

    Update q005_armstrong_number.c

--- LOOP CONSTRUCT/q005_armstrong_number.c
+++ LOOP CONSTRUCT/q005_armstrong_number.c
@@ -1,27 +1,32 @@
-//armstrong number
+// Armstrong number
 #include<stdio.h>
 #include<math.h>
 int main(){
     int num,x,count=0,r=0;
     
-    printf("enter number\n");
+    printf("Enter number : ");
     scanf("%d",&num);
     x=num;
-    for( ;x; ){
+    do{//usind do while for count 0 - as count 1
         x /= 10;
         count++;
-    }
+    }while(x);
     x=num;
-    while (x){
+    while(x){
         r+=pow((x%10),count);
         x/=10;  
     }
     if (r==num)
-        printf("yes, it is armstrong number\n");
+        printf("Yes, it is armstrong number\n");
     else
-        printf("it is not armstrong\n");
-        
-    
-    
+        printf("It is not armstrong\n");
+
 return 0;
 }
+
+/* OUTPUT
+Enter number : 0
+Yes, it is armstrong number
+Enter number : 140
+It is not armstrong
+*/
