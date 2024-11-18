73a5aba5a01c89823f6319435ab692ee8186cf45
d-e-e-p-mandal <deepmandal9091@gmail.com>
2024-11-19 01:05:49 +0530

    Update q016_hollow_diamond_star.c

--- LOOP CONSTRUCT/q016_hollow_diamond_star.c
+++ LOOP CONSTRUCT/q016_hollow_diamond_star.c
@@ -1,48 +1,49 @@
-/*
-print (n=10)
+/* HOLLOW DIAMOND STAR
+
+Enter number of terms : 10
+Holllow Diamond star : 
 ********************
-*********__*********
-********____********
-*******______*******
-******________******
-*****__________*****
-****____________****
-***______________***
-**________________**
-*__________________*
-**________________**
-***______________***
-****____________****
-*****__________*****
-******________******
-*******______*******
-********____********
-*********__*********
+*********  *********
+********    ********
+*******      *******
+******        ******
+*****          *****
+****            ****
+***              ***
+**                **
+*                  *
+**                **
+***              ***
+****            ****
+*****          *****
+******        ******
+*******      *******
+********    ********
+*********  *********
 ********************
 */
- #include<stdio.h>
- //#include<stdlib.h>
- //#include<math.h>
- int main()
- {
-    int n;
-    printf("enter number of terms \n");
-    scanf("%d",&n);
-    for(int i=0; i<2*n-1; i++){
-        int star_1 = (i<n)? n-i : i-(n-2);
-        int space_1 = (i<n)? 2*i : 2*(2*n-i-2);
-        
+#include<stdio.h>
+//#include<stdlib.h>
+int main(){
+   int n;
+   printf("Enter number of terms : ");
+   scanf("%d",&n);
+   printf("Holllow Diamond star : \n");
+   
+   for(int i=0; i<2*n-1; i++){
+       int star_1 = (i<n)? n-i : i-(n-2);
+       int space_1 = (i<n)? i : (2*n-i-2);
 
-        for(int j = 1; j<=star_1; j++){         
-           printf("*");
-        }
-        for( int j=1; j<=space_1; j++){
-           printf(" ");
-        } 
-        for(int j = 1; j<=star_1; j++){         
-           printf("*");
-        }
-        printf("\n");
-    }   
- return 0;
- }
+      for(int j = 1; j<=star_1; j++){         
+         printf("*");
+      }
+      for( int j=1; j<=space_1; j++){
+         printf("  ");
+      } 
+      for(int j = 1; j<=star_1; j++){         
+         printf("*");
+      }
+      printf("\n");
+  }   
+return 0;
+}
