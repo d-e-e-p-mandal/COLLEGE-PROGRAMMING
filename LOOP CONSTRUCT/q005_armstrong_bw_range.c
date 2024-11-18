c00f50d64c8e4adf5ed284a80bd8d7ff06f5a718
d-e-e-p-mandal <deepmandal9091@gmail.com>
2024-11-19 00:54:20 +0530

    Update q005_armstrong_bw_range.c

--- LOOP CONSTRUCT/q005_armstrong_bw_range.c
+++ LOOP CONSTRUCT/q005_armstrong_bw_range.c
@@ -1,32 +1,37 @@
-// armstrong number user input range
+// Armstrong number user input range
 #include <stdio.h>
 #include <math.h>
 int main()
 {
     int num_1, num_2, x, count, r;
 
-    printf("enter range of number\n");
+    printf("Enter range of number : \n");
     scanf("%d%d", &num_1, &num_2);
-    for(int i = num_1; i <= num_2; i++)
-    {
+    printf("Armstrong number between %d to %d :\n",num_1,num_2);
+    for(int i = num_1; i <= num_2; i++){
         x = i;
         r=0;
         count=0;
-        for (; x;)
-        {
+        do{//usind do while for count 0 - as count 1
             x /= 10;
             count++;
-        }
+        }while(x);
         // printf("%d\n",count);
         x = i;
-        while (x)
-        {
+        while(x){
             r+=pow((x%10),count);
             x /= 10;
         }
-            if (r == i)
-                printf("%d\n", i);
+        if (r == i)
+            printf("%d\t", i);
     }
 
     return 0;
 }
+/*OUTPUT
+Enter range of number : 
+0
+1000
+Armstrong number between 0 to 1000 :
+0       1       2       3       4       5       6       7       8       9       153     370     371     407  
+*/
