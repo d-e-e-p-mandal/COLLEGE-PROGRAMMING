e1afc7df7a683f5109ea9510d8170d0edbfa5ea2
d-e-e-p-mandal <deepmandal9091@gmail.com>
2024-11-19 17:32:08 +0530

    Update q001_fibonacci_series.c

--- LOOP CONSTRUCT/q001_fibonacci_series.c
+++ LOOP CONSTRUCT/q001_fibonacci_series.c
@@ -1,35 +1,43 @@
-// fibonacci series
+// Fibonacci series
 #include <stdio.h>
 int main()
 {
     int num;
     int a = 0, b = 1;
-    printf("Enter number  of terms :\n");
+    printf("Enter number  of terms : ");
     scanf("%d", &num);
     if (num < 1)
-    {
         printf("wrong input\n");
-    }
-    else if (num == 1)
-    {
-        printf("%d\n", a);
-    }
-    else if (a == 2)
-    {
-        printf("%d\n", a);
-        printf("%d\n", b);
-    }
-    else
-    {
-        printf("%d\n", a);
-        printf("%d\n", b);
-        for (int i = 3; i <= num; i++)
-        {
-            b = a + b;
-            a = b - a;
+    else{
+        printf("Fibonacci series of %d terms : \n",num);
+        if(num == 1)
+            printf("%d\n", a);
+        else if (a == 2){
+            printf("%d\n", a);
             printf("%d\n", b);
         }
+        else{
+            printf("%d\n", a);
+            printf("%d\n", b);
+            for (int i = 3; i <= num; i++){
+                b = a + b;
+                a = b - a;
+                printf("%d\n", b);
+            }
+        }    
     }
 
     return 0;
 }
+
+/*
+Enter number  of terms : 7
+Fibonacci series of 7 terms : 
+0
+1
+1
+2
+3
+5
+8
+*/
