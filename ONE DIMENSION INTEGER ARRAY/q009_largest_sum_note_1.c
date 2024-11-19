d5fc7b744ffc43e4328ff9b254e8c9d92c077c6b
d-e-e-p-mandal <deepmandal9091@gmail.com>
2024-11-19 18:07:06 +0530

    Update q009_largest_sum_note_1.c

--- ONE DIMENSION INTEGER ARRAY/q009_largest_sum_note_1.c
+++ ONE DIMENSION INTEGER ARRAY/q009_largest_sum_note_1.c
@@ -1,4 +1,4 @@
-// largest sum of contiguous subarray and index 
+// Largest sum of contiguous subarray and index 
 #include<stdio.h>
 int main(){
     int n,i,result,max,x,y;
@@ -24,3 +24,10 @@ int main(){
     printf("Index of sum is %d to %d\n",x,y);
 return 0;
 }
+/*
+Enter the size of array : 8
+Enter the element of array : 
+-2      -3      4       -1      -2      1       5       -3
+Largest sum of contiguous subarray is 7 
+Index of sum is 2 to 6
+*/
