fb8580055009a4fe2a06863871c10b5a4ec77bcc
Deep Mandal <deepmandal9091@gmail.com>
2025-02-24 21:37:52 +0530

    Update 003_cricket_team.c

--- 02 LAB MANUAL/10 STRUCTURE & UNION/003_cricket_team.c
+++ 02 LAB MANUAL/10 STRUCTURE & UNION/003_cricket_team.c
@@ -1,5 +1,4 @@
-/*
-Define a structure called cricket with player name, team name, batting average, for 50 players & 5 teams.
+/* Define a structure called cricket with player name, team name, batting average, for 50 players & 5 teams.
 Print team wise list contains names of player with their batting average.
 */
 #include<stdio.h>
